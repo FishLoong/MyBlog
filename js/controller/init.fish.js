6d80426c29b1849a85d955c54ce443f5e2f040eb
FishLoong <444599785@qq.com>
2017-03-17 17:20:24 +0800

    no message

--- js/controller/init.fish.js
+++ js/controller/init.fish.js
@@ -1,44 +1,55 @@
 var fish=(function($){
-	var buttonStyle=function(index,content,flag,prePage){
-		var $container=$("<span class='nav-bt'></span>");
+	var spanOfBackground={"home":4,"contact":4},
+		btStyleIndex={"home":1,"article":2,"work":2,"message":2,"contact":3},
+		btName=["Home","Article","Work","Message","Contact"],
+		btRemark=[
+			"Fish, just fish. 一只热爱编程与艺术的鱼。",
+			"最新文章：丑鱼 - 梦澧花落知多少",
+			"最新项目：湖南大学易班学生工作站失物招领系统",
+			"期待您留下您的足迹。",
+			"但君所需，愿为驱驰。"
+		];
+	var buttonStyle=function(index,content,flag){
+		var $nav=$("<span class='nav-bt'></span>");
+		var $container=$(".container");
 		if(!flag){
-			$container.on("click",function(){
-				$(".container").removeClass(prePage+"-loaded");
-				console.log(prePage);
+			$nav.on("click",function(){
+				var page=(content.length==2?content[0]:content).toLowerCase();
+				if("pushState" in history){
+					history.pushState(page,"","fish.html");
+				}
+				$container.removeClass($container.attr("class").split(" ")[2]);
 				setTimeout(function(){
-					if(initModule((content.length==2?content[0]:content).toLowerCase())){
+					if(initModule(page)){
 						setTimeout(function(){
-							$(".container").addClass((content.length==2?content[0]:content).toLowerCase()+"-loaded");
+							$container.addClass(page+"-loaded");
+							$(".nav-bt").eq(btName.indexOf((content.length==2?content[0]:content))).addClass("nav-bt-this");
 						},500);
 					}
 				},1000);
 			});
 		}
-		else{
-			$container.addClass("nav-bt-this");
-		}
-		console.log(index+" "+content+" "+flag);
 		switch(index){
 			case 1:
-				$container.html("" +
+				$nav.html("" +
 					"<span class='head'>" +
 						"<span class='text'>"+content[0]+"</span>" +
 						"<span class='image'></span>" +
 					"</span>" +
-					"<span class='tail'>"+content[1]+"</span>");
+					"<span class='tail'><span>"+content[1]+"</span></span>");
 				break;
 			case 2:
-				$container.html("" +
+				$nav.html("" +
 					"<span class='head'></span>" +
 					"<span class='tail'>"+content+"</span>");
 				break;
 			case 3:
-				$container.html("" +
-					content +
-					"<span></span><span></span><span></span><span></span>");
+				$nav.html("" +
+					"<span></span><span></span><span></span><span></span>" +
+					content);
 				break;
 		}
-		return $container;
+		return $nav;
 	};
 	var initModule=function(page){
 		var Page=page[0].toUpperCase()+page.substring(1,page.length),
@@ -47,21 +58,11 @@ var fish=(function($){
 			$nav=$("<section class='nav'></section>");
 		$("title").text("Fish Loong - "+Page);
 		return (function(){
-			var spanOfBackground={"home":4,"contact":4},
-				btStyleIndex={"home":1,"article":2,"work":2,"message":2,"contact":3},
-				btName=["Home","Article","Work","Message","Contact"],
-				btRemark=[
-					"Fish, just fish. 一只热爱编程与艺术的鱼。",
-					"最新文章：丑鱼 - 梦澧花落知多少",
-					"最新项目：湖南大学易班学生工作站失物招领系统",
-					"期待您留下您的足迹。",
-					"但君所需，愿为驱驰。"
-				];
 			for(var i=0,n=spanOfBackground[page];i<n;i++){
 				$("<span></span>").appendTo($background);
 			}
 			for(var i=0;i<5;i++){
-				$nav.append(buttonStyle(btStyleIndex[page],page==="home"?[btName[i],btRemark[i]]:btName[i],Page===btName[i],page));
+				$nav.append(buttonStyle(btStyleIndex[page],page==="home"?[btName[i],btRemark[i]]:btName[i],btName[i]===Page));
 			}
 			$container.empty().attr("class","container "+page).append($background).append($nav).append($("" +
 				"<section class='copyright'>Copyright©longzeming1996@gmail.com, Jeremy Loong</section>"));
--- js/index.js
+++ js/index.js
@@ -1,10 +1,31 @@
-if("transition" in document.documentElement.style){
+if(!(!!window.ActiveXObject || "ActiveXObject" in window)){
+	window.pageIndex=["home","article","work","message","contact"];
 	window.onload=function(){
 		console.log(new Date()-t);
+		if("pushState" in history){
+			$(window).on("popstate",function(){
+				var state=history.state;
+				if(state){
+					var $container=$(".container");
+					var delClass=$container.attr("class").split(" ")[2];
+					$container.removeClass(delClass);
+					setTimeout(function(){
+						if(fish.initModule(state)){
+							setTimeout(function(){
+								$container.addClass(state+"-loaded");
+								$(".nav-bt").eq(pageIndex.indexOf(state)).addClass("nav-bt-this");
+							},500);
+						}
+					},1000);
+				}
+			});
+			history.pushState("home","","fish.html");
+		}
 		if(fish.initModule("home")){
 			setTimeout(function(){
 				console.log(new Date()-t);
 				$(".container").addClass("home-loaded");
+				$(".nav-bt").eq(0).addClass("nav-bt-this");
 			},700);
 		}
 	}
